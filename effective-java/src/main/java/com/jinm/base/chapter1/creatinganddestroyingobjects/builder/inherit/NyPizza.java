858b41702a48aa72d397e8f102125ed29c8f3987
jinminer <keanemer@gmail.com>
2020-02-14 00:04:17 +0800

    append builder pattern in effective-java

---
+++ effective-java/src/main/java/com/jinm/base/chapter1/creatinganddestroyingobjects/builder/inherit/NyPizza.java
@@ -0,0 +1,24 @@
+package com.jinm.base.chapter1.creatinganddestroyingobjects.builder.inherit;
+
+import java.util.Objects;
+
+public class NyPizza extends Pizza {
+    public enum Size { SMALL, MEDIUM, LARGE }
+    private final Size size;
+    public static class Builder extends Pizza.Builder<Builder> {
+        private final Size size;
+        public Builder(Size size) {
+            this.size = Objects.requireNonNull(size);
+        }
+        @Override public NyPizza build() {
+            return new NyPizza(this);
+        }
+        @Override protected Builder self() {
+            return this;
+        }
+    }
+    private NyPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+}
