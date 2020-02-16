5209c1501f89a03256add33118156c8362b425df
jinminer <keanemer@gmail.com>
2020-02-17 01:40:39 +0800

    add content

---
+++ effective-java/src/main/java/com/jinm/base/chapter3/classesandinterfaces/combineinherit/MyHashSet.java
@@ -0,0 +1,15 @@
+package com.jinm.base.chapter3.classesandinterfaces.combineinherit;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MyHashSet {
+
+    public static void main(String[] args) {
+
+        HashSet<String> set = new HashSet<>();
+        set.addAll(new ArrayList<String>());
+
+    }
+
+}
