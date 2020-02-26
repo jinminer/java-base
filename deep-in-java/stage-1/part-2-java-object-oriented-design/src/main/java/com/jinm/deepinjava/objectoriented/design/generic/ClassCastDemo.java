6190741cfa436458bfd7d6e3ec024ff1742e5071
jinminer <keanemer@gmail.com>
2020-02-27 01:20:40 +0800

    append generic content

---
+++ deep-in-java/stage-1/part-2-java-object-oriented-design/src/main/java/com/jinm/deepinjava/objectoriented/design/generic/ClassCastDemo.java
@@ -0,0 +1,19 @@
+package com.jinm.deepinjava.objectoriented.design.generic;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassCastDemo {
+
+    public static void main(String[] args) {
+
+        Type type = int.class;
+        Class classType = int.class;
+
+        //Diamond 语法
+        List<String> list = new ArrayList<>();
+
+    }
+
+}
