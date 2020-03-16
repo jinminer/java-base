774033317c48fb56102ea95bd407031078327207
jinminer <keanemer@gmail.com>
2020-03-16 23:58:17 +0800

    add ClassLoader and ClassPath, Class loading

---
+++ deep-in-java/stage-5-java-storage-management/part-1-java-class-hierarchy/src/main/java/com/jinm/deepinjava/classhierarchy/User.java
@@ -0,0 +1,18 @@
+package com.jinm.deepinjava.classhierarchy;
+
+
+/**
+ * V1 版本拥有 ID 字段
+ */
+public class User {
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
